943159c75897e98256f6a4fa4891744a425f8962
nanthicha2001 <nanthichakassawang.2001@gmail.com>
2025-05-13 14:46:15 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- nav.php -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/phrae/index.php">Phrae Province</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="/phrae/index.php">หน้าแรก</a></li>
+                <li class="nav-item"><a class="nav-link" href="/phrae/category/province">ข้อมูลท่องเที่ยวจังหวัด</a></li>
+                <li class="nav-item"><a class="nav-link" href="/phrae/category/travelprogram">เพลนเที่ยว</a></li>
+                <li class="nav-item"><a class="nav-link" href="/phrae/category/travel_db">สถานที่ท่องเที่ยว</a></li>
+                <li class="nav-item"><a class="nav-link" href="/phrae/category/restaurant">คาเฟ่ ร้านอาหาร</a></li>
+                <li class="nav-item"><a class="nav-link" href="/phrae/category/accommodation">ที่พัก</a></li>
+            </ul>
+            <div class="d-flex">
+                <a class="nav-link m-1" href="/Phrae/admin/signin.php">LogIn <i class="bi bi-person-fill"></i></a>
+            </div>
+        </div>
+    </div>
+</nav>
